d32fd6d592eb88193ef0d8596e913f09784cb318
hywax <me@hywax.space>
2024-02-06 21:06:21 +0500

    chore: add vite builder

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { builtinModules } from 'node:module'
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
+
+const external = [...builtinModules, ...builtinModules.map((m) => `node:${m}`)]
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      fileName: 'index',
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      external,
+      output: {
+        inlineDynamicImports: true,
+      },
+      preserveSymlinks: true,
+    },
+  },
+})
