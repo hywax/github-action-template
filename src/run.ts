52537aa2816e085738d85952d3f81bff3cc3f68f
hywax <me@hywax.space>
2024-02-06 21:05:22 +0500

    chore: change type import

--- src/run.ts
+++ src/run.ts
@@ -1,18 +1,17 @@
-import * as core from '@actions/core'
-import * as github from '@actions/github'
+import { debug, getInput, setFailed, setOutput } from '@actions/core'
+import { context } from '@actions/github'
 
 export async function run(): Promise<void> {
   try {
-    const { context } = github
-    const message: string = core.getInput('message')
+    const message: string = getInput('message')
 
-    core.debug(`Event: ${context.eventName}, Repo: ${context.repo.repo}, SHA: ${context.sha}`)
-    core.debug(`${message}`)
+    debug(`Event: ${context.eventName}, Repo: ${context.repo.repo}, SHA: ${context.sha}`)
+    debug(`${message}`)
 
-    core.setOutput('time', 10)
+    setOutput('time', 10)
   } catch (error) {
     if (error instanceof Error) {
-      core.setFailed(error.message)
+      setFailed(error.message)
     }
   }
 }
