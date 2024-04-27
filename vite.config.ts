1df34e162ffcb12acb2bd0016320ab7008d5d743
hywax <me@hywax.space>
2024-04-27 17:50:35 +0500

    build: change formats

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       fileName: 'index',
-      formats: ['es', 'cjs'],
+      formats: ['es'],
     },
     rollupOptions: {
       external,
